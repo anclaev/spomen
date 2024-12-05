36b6b7e35f4bcccf208f4c856c75bdfcc1b4ccce
anclaev <iahugo@yandex.ru>
2024-12-05 03:51:23 +0300

    fix(ci): cI
    настройка CI

--- scripts/generate-proto.sh
+++ scripts/generate-proto.sh
@@ -10,6 +10,7 @@ npx protoc --plugin=node_modules/ts-proto/$PLUGIN \
     --ts_proto_opt=nestJs=true \
     --ts_proto_opt=returnObservable=false  \
     --ts_proto_opt=addGrpcMetadata=true \
+    --ts_proto_opt=env=node \
     --ts_proto_out=./libs/core/src/lib/grpc \
     -I=./apps/account/src \
     ./apps/account/src/account.proto \
